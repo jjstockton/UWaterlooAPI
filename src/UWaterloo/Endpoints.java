5160926d5abbea655b6a0d7966f43197b182d562
Jacob Stockton <jacob.stockton@hotmail.com>
2016-09-24 03:08:16 -0400

    added rest of endpoints

--- src/UWaterloo/Endpoints.java
+++ src/UWaterloo/Endpoints.java
@@ -58,8 +58,72 @@ public enum Endpoints {
     SERVICES_SITE("/servies/{site}"),
 
     // Weather
-    WEATHER_CURRENT("/weather/current");
+    WEATHER_CURRENT("/weather/current"),
 
+    // Terms
+    TERMS_LIST("/terms/list"),
+    TERMS_TERM_COURSES("/terms/{term}/courses"),
+    TERMS_TERM_EXAMSCHEDULE("/terms/{term}/examschedule"),
+    TERMS_TERM_SUBJECT_SCHEDULE("/terms/{term}/{subject}/schedule"),
+    TERMS_TERM_SUBJECT_CATALOGNUMBER_SCHEDULE("/terms/{term}/{subject}/{catalog_number}/schedule"),
+    TERMS_TERM_ENROLLMENT("/terms/{term}/enrollment"),
+    TERMS_TERM_SUBJECT_ENROLLMENT("/terms/{term}/{subject}/enrollment"),
+    TERMS_TERM_INFOSESSIONS("/terms/{term}/infosessions"),
+
+    // Resources
+    RESOURCES_TUTORS("/resources/tutors"),
+    RESOURCES_PRINTERS("/resources/printers"),
+    RESOURCES_INFOSESSIONS("/resources/infosessions"),
+    RESOURCES_GOOSEWATCH("/resources/goosewatch"),
+    RESOURCES_SUNSHINELIST("/resources/sunshinelist"),
+
+    // Definitions and Codes
+    CODES_UNITS("/codes/units"),
+    CODES_TERMS("/codes/terms"),
+    CODES_GROUPS("/codes/groups"),
+    CODES_SUBJETS("/codes/subjects"),
+    CODES_INSTRUCTIONS("/codes/instructions"),
+
+    // Building
+    BUILDINGS_LIST("/buildings/list"),
+    BUILDINGS_BUILDINGCODE("/buildings/{building_code}"),
+    BUILDINGS_BUILDING_ROOM_COURSES("/buildings/{building}/{room}/courses"),
+    BUILDINGS_BUILDINGCODE_ACCESSPOINTS("/building/{building_code}/accesspoints"),
+    BUILDINGS_BUILDINGCODE_VENDINGMACHINES("/buildings/{building_code}/vendingmachines"),
+
+    // Points of Interest
+    POI_ATMS("/poi/atms"),
+    POI_GREYHOUND("/poi/greyhound"),
+    POI_HELPLINES("/poi/helplines"),
+    POI_LIBRARIES("/poi/libraries"),
+    POI_PHOTOSPHERES("/poi/photospheres"),
+    POI_DEFIBRILLATORS("/poi/defibrillators"),
+    POI_CONSTRUCTIONSITES("/poi/constructionsites"),
+    POI_ACCESSIBLEENTRANCES("/poi/accessibleentrances"),
+    POI_VISITORINFORMATION("/poi/visitorinformation"),
+
+    // Parking
+    PARKING_WATPARK("/parking/watpark"),
+    PARKING_LOTS_METER("/parking/lots/meter"),
+    PARKING_LOTS_PERMIT("/parking/lots/permit"),
+    PARKING_LOTS_VISITOR("/parking/lots/visitor"),
+    PARKING_LOTS_SHORTTERM("/parking/lots/shortterm"),
+    PARKING_LOTS_ACCESSIBLE("/parking/lots/accessible"),
+    PARKING_LOTS_MOTORCYCLE("/parking/lots/motorcycle"),
+
+    // Transit
+    TRANSIT_GRT("/transit/grt"),
+    TRANSIT_GRT_STOPS("/transit/grt/stops"),
+
+    // People Directory Search
+    DIRECTORY_USERID("/directory/{user_id}"),
+
+    // API
+    API_USAGE("/api/usage"),
+    API_SERVICES("/api/services"),
+    API_METHODS("/api/methods"),
+    API_VERSIONS("/api/versions"),
+    API_CHANGELOGS("/api/changelogs");
 
     final String uri;
     Endpoints(String uri) {
---
+++ src/UWaterloo/StringUtil.java
@@ -0,0 +1,7 @@
+package UWaterloo;
+
+/**
+ * Created by user on 2016-09-24.
+ */
+public class StringUtil {
+}
