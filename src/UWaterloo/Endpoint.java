f98b5ef143cad455d72ea2ad2b7dbf036e1fb8a9
Jacob Stockton <jacob.stockton@rubikloud.com>
2016-09-28 13:38:34 -0400

    added Endpoint class implementation

--- src/UWaterloo/Endpoint.java
+++ src/UWaterloo/Endpoint.java
@@ -1,7 +1,55 @@
 package UWaterloo;
 
-/**
- * Created by user on 2016-09-24.
- */
+import org.json.JSONObject;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static UWaterloo.JsonUtils.getJson;
+
 public class Endpoint {
+
+    private static final String BASE_URL = "https://api.uwaterloo.ca/v2";
+
+    private final String url;
+    final Class c;
+
+    Endpoint(String uri, Class c) {
+        this.url = BASE_URL + uri + ".json";
+        this.c = c;
+    }
+
+    Object getData(String[] args, String key) {
+
+        String authenticatedUrl = fillArguments(args) + "?key=" + key;
+
+        System.out.println(authenticatedUrl);
+
+        JSONObject jsonObject = getJson(authenticatedUrl).getJSONObject("data");
+
+        Deserializer d = new Deserializer(this.c);
+
+        return d.deserialize(jsonObject);
+
+    }
+
+    private String fillArguments(String... args) {
+
+        String filledUrl = this.url;
+        Pattern p = Pattern.compile("\\{.*?\\}");
+        Matcher m = p.matcher(this.url);
+
+        int i;
+        for(i = 0; m.find(); i++) {
+            filledUrl = filledUrl.replaceFirst("\\{.*?\\}", args[i]);
+        }
+
+        if(i < args.length) {
+            throw new IllegalArgumentException();
+        }
+
+        return filledUrl;
+
+    }
+
 }
--- src/UWaterloo/Endpoints.java
+++ src/UWaterloo/Endpoints.java
@@ -3,131 +3,136 @@ package UWaterloo;
 public enum Endpoints {
 
     // Food Services
-    FOODSERVICES_MENU("/foodservices/menu"),
-    FOODSERVICES_NOTES("/foodservices/notes"),
-    FOODSERVICES_DIETS("/foodservices/diets"),
-    FOODSERVICES_OUTLETS("/foodservices/outlets"),
-    FOODSERVICES_LOCATIONS("/foodservices/locations"),
-    FOODSERVICES_WATCARD("/foodservices/watcard"),
-    FOODSERVICES_ANNOUNCEMENTS("/foodservices/announcements"),
-    FOODSERVICES_PRODUCTS_PRODUCTID("/foodservices/products/product_id"),
-    FOODSERVICES_YEAR_WEEK_MENU("/foodservices/{year}/{week}/menu"),
-    FOODSERVICES_YEAR_WEEK_NOTES("/foodservices/{year}/{week}/notes"),
-    FOODSERVICES_YEAR_WEEK_ANNOUNCEMENTS("/foodservices/{year}/{week}/announcements"),
+    FOODSERVICES_MENU("/foodservices/menu", Object.class),
+    FOODSERVICES_NOTES("/foodservices/notes", Object.class),
+    FOODSERVICES_DIETS("/foodservices/diets", Object.class),
+    FOODSERVICES_OUTLETS("/foodservices/outlets", Object.class),
+    FOODSERVICES_LOCATIONS("/foodservices/locations", Object.class),
+    FOODSERVICES_WATCARD("/foodservices/watcard", Object.class),
+    FOODSERVICES_ANNOUNCEMENTS("/foodservices/announcements", Object.class),
+    FOODSERVICES_PRODUCTS_PRODUCTID("/foodservices/products/product_id", Object.class),
+    FOODSERVICES_YEAR_WEEK_MENU("/foodservices/{year}/{week}/menu", Object.class),
+    FOODSERVICES_YEAR_WEEK_NOTES("/foodservices/{year}/{week}/notes", Object.class),
+    FOODSERVICES_YEAR_WEEK_ANNOUNCEMENTS("/foodservices/{year}/{week}/announcements", Object.class),
 
     //FEDS
-    FEDS_EVENTS("/feds/events"),
-    FEDS_EVENTS_ID("/feds/events/{id}"),
-    FEDS_LOCATIONS("/feds/locations"),
+    FEDS_EVENTS("/feds/events", Object.class),
+    FEDS_EVENTS_ID("/feds/events/{id}", Object.class),
+    FEDS_LOCATIONS("/feds/locations", Object.class),
 
     // Course
-    COURSES("/courses"),
-    COURSES_SUBJECT("/courses/{subject}"),
-    COURSES_COURSEID("/courses/{course_id}"),
-    COURSES_CLASSNUMBER_SCHEDULE("/courses/{class_number}/schedule"),
-    COURSES_SUBJECT_CATALOGNUMBER("/courses/{subject}/{catalog_number}"),
-    COURSES_SUBJECT_CATALOGNUMBER_SCHEDULE("/courses/{subject}/{catalog_number}/schedule"),
-    COURSES_SUBJECT_CATALOGNUMBER_PREREQUISITES("/courses/{subject}/{catalog_number}/prerequisites"),
-    COURSES_SUBJECT_CATALOGNUMBER_EXAMSCHEDULE("/courses/{subject}/{catalog_number}/examschedule"),
+    COURSES("/courses", Object.class),
+    COURSES_SUBJECT("/courses/{subject}", Course.class),
+    COURSES_COURSEID("/courses/{course_id}", Object.class),
+    COURSES_CLASSNUMBER_SCHEDULE("/courses/{class_number}/schedule", Object.class),
+    COURSES_SUBJECT_CATALOGNUMBER("/courses/{subject}/{catalog_number}", Course.class),
+    COURSES_SUBJECT_CATALOGNUMBER_SCHEDULE("/courses/{subject}/{catalog_number}/schedule", Object.class),
+    COURSES_SUBJECT_CATALOGNUMBER_PREREQUISITES("/courses/{subject}/{catalog_number}/prerequisites", Object.class),
+    COURSES_SUBJECT_CATALOGNUMBER_EXAMSCHEDULE("/courses/{subject}/{catalog_number}/examschedule", Object.class),
 
     // Awards/Scholarships
-    AWARDS_GRADUATE("/awards/graduate"),
-    AWARDS_UNDERGRADUATE("/awards/undergraduate"),
+    AWARDS_GRADUATE("/awards/graduate", Object.class),
+    AWARDS_UNDERGRADUATE("/awards/undergraduate", Object.class),
 
     // Events
-    EVENTS("/events"),
-    EVENTS_SITE("/events/{site}"),
-    EVENTS_SITE_ID("/events/{site}/{id}"),
-    EVENTS_HOLIDAYS("/events/holidays"),
+    EVENTS("/events", Object.class),
+    EVENTS_SITE("/events/{site}", Object.class),
+    EVENTS_SITE_ID("/events/{site}/{id}", Object.class),
+    EVENTS_HOLIDAYS("/events/holidays", Object.class),
 
     // Blogs
-    BLOGS_SITE("/blogs/{site}"),
-    BLOGS_SITE_ID("/blogs/{site}/{id}"),
+    BLOGS_SITE("/blogs/{site}", Object.class),
+    BLOGS_SITE_ID("/blogs/{site}/{id}", Object.class),
 
     // News
-    NEWS("/news"),
-    NEWS_SITE("/news/{site}"),
-    NEWS_SITE_ID("/news/{site}/{id}"),
+    NEWS("/news", Object.class),
+    NEWS_SITE("/news/{site}", Object.class),
+    NEWS_SITE_ID("/news/{site}/{id}", Object.class),
 
     // Opportunities/Jobs
-    OPPORTUNITIES("/opportunities"),
-    OPPORTUNITIES_SITE("/opportunities/{site}"),
-    OPPORTUNITIES_SITE_ID("/opportunities/{site}/{id}"),
+    OPPORTUNITIES("/opportunities", Object.class),
+    OPPORTUNITIES_SITE("/opportunities/{site}", Object.class),
+    OPPORTUNITIES_SITE_ID("/opportunities/{site}/{id}", Object.class),
 
     // Services
-    SERVICES_SITE("/servies/{site}"),
+    SERVICES_SITE("/servies/{site}", Object.class),
 
     // Weather
-    WEATHER_CURRENT("/weather/current"),
+    WEATHER_CURRENT("/weather/current", Object.class),
 
     // Terms
-    TERMS_LIST("/terms/list"),
-    TERMS_TERM_COURSES("/terms/{term}/courses"),
-    TERMS_TERM_EXAMSCHEDULE("/terms/{term}/examschedule"),
-    TERMS_TERM_SUBJECT_SCHEDULE("/terms/{term}/{subject}/schedule"),
-    TERMS_TERM_SUBJECT_CATALOGNUMBER_SCHEDULE("/terms/{term}/{subject}/{catalog_number}/schedule"),
-    TERMS_TERM_ENROLLMENT("/terms/{term}/enrollment"),
-    TERMS_TERM_SUBJECT_ENROLLMENT("/terms/{term}/{subject}/enrollment"),
-    TERMS_TERM_INFOSESSIONS("/terms/{term}/infosessions"),
+    TERMS_LIST("/terms/list", Object.class),
+    TERMS_TERM_COURSES("/terms/{term}/courses", Object.class),
+    TERMS_TERM_EXAMSCHEDULE("/terms/{term}/examschedule", Object.class),
+    TERMS_TERM_SUBJECT_SCHEDULE("/terms/{term}/{subject}/schedule", Object.class),
+    TERMS_TERM_SUBJECT_CATALOGNUMBER_SCHEDULE("/terms/{term}/{subject}/{catalog_number}/schedule", Object.class),
+    TERMS_TERM_ENROLLMENT("/terms/{term}/enrollment", Object.class),
+    TERMS_TERM_SUBJECT_ENROLLMENT("/terms/{term}/{subject}/enrollment", Object.class),
+    TERMS_TERM_INFOSESSIONS("/terms/{term}/infosessions", Object.class),
 
     // Resources
-    RESOURCES_TUTORS("/resources/tutors"),
-    RESOURCES_PRINTERS("/resources/printers"),
-    RESOURCES_INFOSESSIONS("/resources/infosessions"),
-    RESOURCES_GOOSEWATCH("/resources/goosewatch"),
-    RESOURCES_SUNSHINELIST("/resources/sunshinelist"),
+    RESOURCES_TUTORS("/resources/tutors", Object.class),
+    RESOURCES_PRINTERS("/resources/printers", Object.class),
+    RESOURCES_INFOSESSIONS("/resources/infosessions", Object.class),
+    RESOURCES_GOOSEWATCH("/resources/goosewatch", Object.class),
+    RESOURCES_SUNSHINELIST("/resources/sunshinelist", Object.class),
 
     // Definitions and Codes
-    CODES_UNITS("/codes/units"),
-    CODES_TERMS("/codes/terms"),
-    CODES_GROUPS("/codes/groups"),
-    CODES_SUBJETS("/codes/subjects"),
-    CODES_INSTRUCTIONS("/codes/instructions"),
+    CODES_UNITS("/codes/units", Object.class),
+    CODES_TERMS("/codes/terms", Object.class),
+    CODES_GROUPS("/codes/groups", Object.class),
+    CODES_SUBJETS("/codes/subjects", Object.class),
+    CODES_INSTRUCTIONS("/codes/instructions", Object.class),
 
     // Building
-    BUILDINGS_LIST("/buildings/list"),
-    BUILDINGS_BUILDINGCODE("/buildings/{building_code}"),
-    BUILDINGS_BUILDING_ROOM_COURSES("/buildings/{building}/{room}/courses"),
-    BUILDINGS_BUILDINGCODE_ACCESSPOINTS("/building/{building_code}/accesspoints"),
-    BUILDINGS_BUILDINGCODE_VENDINGMACHINES("/buildings/{building_code}/vendingmachines"),
+    BUILDINGS_LIST("/buildings/list", Object.class),
+    BUILDINGS_BUILDINGCODE("/buildings/{building_code}", Object.class),
+    BUILDINGS_BUILDING_ROOM_COURSES("/buildings/{building}/{room}/courses", Object.class),
+    BUILDINGS_BUILDINGCODE_ACCESSPOINTS("/building/{building_code}/accesspoints", Object.class),
+    BUILDINGS_BUILDINGCODE_VENDINGMACHINES("/buildings/{building_code}/vendingmachines", Object.class),
 
     // Points of Interest
-    POI_ATMS("/poi/atms"),
-    POI_GREYHOUND("/poi/greyhound"),
-    POI_HELPLINES("/poi/helplines"),
-    POI_LIBRARIES("/poi/libraries"),
-    POI_PHOTOSPHERES("/poi/photospheres"),
-    POI_DEFIBRILLATORS("/poi/defibrillators"),
-    POI_CONSTRUCTIONSITES("/poi/constructionsites"),
-    POI_ACCESSIBLEENTRANCES("/poi/accessibleentrances"),
-    POI_VISITORINFORMATION("/poi/visitorinformation"),
+    POI_ATMS("/poi/atms", Object.class),
+    POI_GREYHOUND("/poi/greyhound", Object.class),
+    POI_HELPLINES("/poi/helplines", Object.class),
+    POI_LIBRARIES("/poi/libraries", Object.class),
+    POI_PHOTOSPHERES("/poi/photospheres", Object.class),
+    POI_DEFIBRILLATORS("/poi/defibrillators", Object.class),
+    POI_CONSTRUCTIONSITES("/poi/constructionsites", Object.class),
+    POI_ACCESSIBLEENTRANCES("/poi/accessibleentrances", Object.class),
+    POI_VISITORINFORMATION("/poi/visitorinformation", Object.class),
 
     // Parking
-    PARKING_WATPARK("/parking/watpark"),
-    PARKING_LOTS_METER("/parking/lots/meter"),
-    PARKING_LOTS_PERMIT("/parking/lots/permit"),
-    PARKING_LOTS_VISITOR("/parking/lots/visitor"),
-    PARKING_LOTS_SHORTTERM("/parking/lots/shortterm"),
-    PARKING_LOTS_ACCESSIBLE("/parking/lots/accessible"),
-    PARKING_LOTS_MOTORCYCLE("/parking/lots/motorcycle"),
+    PARKING_WATPARK("/parking/watpark", Object.class),
+    PARKING_LOTS_METER("/parking/lots/meter", Object.class),
+    PARKING_LOTS_PERMIT("/parking/lots/permit", Object.class),
+    PARKING_LOTS_VISITOR("/parking/lots/visitor", Object.class),
+    PARKING_LOTS_SHORTTERM("/parking/lots/shortterm", Object.class),
+    PARKING_LOTS_ACCESSIBLE("/parking/lots/accessible", Object.class),
+    PARKING_LOTS_MOTORCYCLE("/parking/lots/motorcycle", Object.class),
 
     // Transit
-    TRANSIT_GRT("/transit/grt"),
-    TRANSIT_GRT_STOPS("/transit/grt/stops"),
+    TRANSIT_GRT("/transit/grt", Object.class),
+    TRANSIT_GRT_STOPS("/transit/grt/stops", Object.class),
 
     // People Directory Search
-    DIRECTORY_USERID("/directory/{user_id}"),
+    DIRECTORY_USERID("/directory/{user_id}", Object.class),
 
     // API
-    API_USAGE("/api/usage"),
-    API_SERVICES("/api/services"),
-    API_METHODS("/api/methods"),
-    API_VERSIONS("/api/versions"),
-    API_CHANGELOGS("/api/changelogs");
+    API_USAGE("/api/usage", Object.class),
+    API_SERVICES("/api/services", Object.class),
+    API_METHODS("/api/methods", Object.class),
+    API_VERSIONS("/api/versions", Object.class),
+    API_CHANGELOGS("/api/changelogs", Object.class);
 
-    final String uri;
-    Endpoints(String uri) {
-        this.uri = uri;
+
+    private final Endpoint e;
+
+    Endpoints(String uri, Class c) {
+        e = new Endpoint(uri, c);
     }
 
+    Object getData(String[] args, String key) {
+        return e.getData(args, key);
+    }
 }
